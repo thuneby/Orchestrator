26f47a211eeb84b7caa2d583111be199edc616ab
John Thuneby <thuneby@yahoo.dk>
2022-09-13 17:57:44 +0200

    Ignore eventbus test

--- test/PersistanceTest/EventBus/RedisEventBusTests.cs
+++ test/PersistanceTest/EventBus/RedisEventBusTests.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.Models;
+﻿using Core.Models;
 using Core.QueueModels;
 using EventBus.Abstractions;
-using EventBus.Extensions;
-using Microsoft.Extensions.Logging;
 using PersistanceTest.Common;
 
 namespace PersistanceTest.EventBus
 {
     public class RedisEventBusTests: TestBase
     {
-        private readonly IEventBus _eventBus;
+        private IEventBus _eventBus;
 
         public RedisEventBusTests()
         {
             Initialize();
-            _eventBus = new RedisEventBus(TestLoggerFactory.CreateLogger<RedisEventBus>());
         }
-
-        [Fact]
+        
+        [Fact(Skip = "Requires EventBus")]
+        // Requirres eventBus
         public void PublishEvent()
         {
+            //_eventBus = new RedisEventBus(TestLoggerFactory.CreateLogger<RedisEventBus>());
             var message = new QueueMessage(Guid.NewGuid(), DocumentType.NetsOsInfo, ProcessState.Receive,
                 "testfile.txt", "");
         }
