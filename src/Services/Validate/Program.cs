8e7af97c4e54020114c8ed31dbe1b6db51a39a81
John Thuneby <thuneby@yahoo.dk>
2022-10-10 15:03:28 +0200

    Added MasterDataRepository to Validate DI

--- src/Services/Validate/Program.cs
+++ src/Services/Validate/Program.cs
@@ -1,6 +1,9 @@
+using DataAccess.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<MasterDataRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
