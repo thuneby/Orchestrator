af5bebdec831ed2de6740283ee90312397f11f24
thuneby <thuneby@yahoo.dk>
2023-02-14 14:33:42 +0100

    Update resourcegroups.tf

--- terraform/resourcegroups.tf
+++ terraform/resourcegroups.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "orchestrator" {
-    name        = var.resource_group
-    location    = var.location
+  name     = var.resource_group
+  location = var.location
 }
