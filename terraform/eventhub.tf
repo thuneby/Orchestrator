4e26960e899703071ca60e4e2a2dfbfdb079725c
thuneby <thuneby@yahoo.dk>
2023-02-14 13:52:26 +0100

    Update eventhub.tf

--- terraform/eventhub.tf
+++ terraform/eventhub.tf
@@ -1,4 +1,4 @@
-resource "azurecaf_name" "eventhub_name" {
+resource "azurecaf_name" "eventhub_namespace" {
   name          = "eventhub-orchestrator"
   resource_type = "azurerm_servicebus_namespace"
   suffixes      = ["sharperbox"]
@@ -6,8 +6,17 @@ resource "azurecaf_name" "eventhub_name" {
 }
 
 resource "azurerm_eventhub_namespace" "eventhub_namespace" {
-    name = azurecaf_name.eventhub_name.result
-    resource_group_name = azurerm_resource_group.orchestrator.name
-    location = azurerm_resource_group.orchestrator.location
-    sku = "Standard"
+  name                = azurecaf_name.eventhub_namespace.result
+  resource_group_name = azurerm_resource_group.orchestrator.name
+  location            = azurerm_resource_group.orchestrator.location
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+resource "azurerm_eventhub" "hub" {
+  name                = "events"
+  namespace_name    = azurecaf_name.eventhub_namespace.result
+  resource_group_name = azurerm_resource_group.orchestrator.name
+  partition_count     = 2
+  message_retention   = 1
 }
