ce9d9ef05ffae440eb2074d0c219039b4be2f099
thuneby <thuneby@yahoo.dk>
2023-02-14 10:43:46 +0100

    Create servicebus.tf

---
+++ terraform/servicebus.tf
@@ -0,0 +1,14 @@
+resource "azurecaf_name" "servicebus_name" {
+  name          = "sb-orchestrator"
+  resource_type = "azurerm_servicebus_namespace"
+  suffixes      = "sharperbox"
+  clean_input   = true
+}
+
+resource "azurerm_servicebus_namespace" "servicebus" {
+  name                = azurecaf_name.servicebus_name.result
+  location            = var.Location
+  resource_group_name = var.ResourceGroup
+  sku                 = "Standard"
+  capacity            = 1
+}
