5308bc28a089a967b257ba54d04f0f66fe7cf5e6
thuneby <thuneby@yahoo.dk>
2023-02-14 14:32:42 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,20 +1,19 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-        }
-        azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "~> 1.2.5"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
     }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+    }
+  }
   
-    backend "azurerm" {
-        resource_group_name  = "Terraform-state"
-        storage_account_name = "orchestratorterraform"
-        container_name       = "tfstate"
-        key                  = "tfstate"
-        }
+  backend "azurerm" {
+    resource_group_name  = "Terraform-state"
+    storage_account_name = "orchestratorterraform"
+    container_name       = "tfstate"
+    key                  = "tfstate"
+  }
 }
 provider "azurerm" {
   features {}
