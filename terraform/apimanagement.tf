8dfa10ee4eee286cbac3515890338442415d0c9a
thuneby <thuneby@yahoo.dk>
2023-02-14 14:55:53 +0100

    Update apimanagement.tf

--- terraform/apimanagement.tf
+++ terraform/apimanagement.tf
@@ -5,11 +5,11 @@ resource "azurecaf_name" "apim_name" {
   clean_input   = true
 }
 
-resource "azurerm_api_management" "apim" {
-  name                = azurecaf_name.apim_name.result
-  location            = azurerm_resource_group.orchestrator.location
-  resource_group_name = azurerm_resource_group.orchestrator.name
-  publisher_name      = "Sharperbox"
-  publisher_email     = "api@sharperbox.dk"
-  sku_name            = "Consumption_0"
-}
+# resource "azurerm_api_management" "apim" {
+#   name                = azurecaf_name.apim_name.result
+#   location            = azurerm_resource_group.orchestrator.location
+#   resource_group_name = azurerm_resource_group.orchestrator.name
+#   publisher_name      = "Sharperbox"
+#   publisher_email     = "api@sharperbox.dk"
+#   sku_name            = "Consumption_0"
+# }
